43a08edcc6d44edc03a5e000f0505d033c850cac
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-06 17:49:17 +0000

    Add more to calculator

--- my-app/src/main/java/com/linkedin/app/Calculator.java
+++ my-app/src/main/java/com/linkedin/app/Calculator.java
@@ -5,4 +5,12 @@ public class Calculator {
   public int add(int a, int b) {
     return a + b;
   }
+
+  public int divide(int a, int b) {
+    return a / b;
+  }
+
+  public boolean isEven(int n) {
+    return n % 2 == 0;
+  }
 }
