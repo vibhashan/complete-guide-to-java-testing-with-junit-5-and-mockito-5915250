7df245d151cd3d060ec734ae4e89c0e82ea411b4
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-06 22:30:51 +0000

    03_07 implement

--- my-app/src/main/java/com/linkedin/app/Task.java
+++ my-app/src/main/java/com/linkedin/app/Task.java
@@ -3,14 +3,20 @@ package com.linkedin.app;
 public class Task {
 
   private Integer id;
+  private String description;
   private static int counter = 0;
 
-  public Task() {
+  public Task(String description) {
     this.id = counter++;
+    this.description = description;
   }
 
   public Integer getId() {
     return id;
+  }
+
+  public String getDescription() {
+    return description;
   }
 
 }
