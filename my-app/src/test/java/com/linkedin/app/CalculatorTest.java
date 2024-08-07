28a7b027ab34e3d39b6e1b6e423ea3712b9eddc3
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-07 18:21:19 +0000

    Set up

---
+++ my-app/src/test/java/com/linkedin/app/CalculatorTest.java
@@ -0,0 +1,30 @@
+package com.linkedin.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void add() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void subtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    public void multiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void divide() {
+        assertEquals(2.0, calculator.divide(4, 2));
+    }
+}
