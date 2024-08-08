3631b66b1bd2fb4019eb92efd4765f6edc25bf73
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-08 19:07:52 +0000

    End state

--- my-app/src/test/java/com/linkedin/app/UserNotificationServiceTest.java
+++ my-app/src/test/java/com/linkedin/app/UserNotificationServiceTest.java
@@ -1,5 +1,12 @@
 package com.linkedin.app;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Test;
@@ -25,11 +32,26 @@ public class UserNotificationServiceTest {
 
     @Test
     void notifyUser() throws EmailException {
+        underTest.notifyUser(EMAIL, MESSAGE);
 
+        verify(emailService).sendEmail(EMAIL, "Notification", MESSAGE);
+        verify(logger).info("Attempting to send notification to " + EMAIL);
+        verify(logger).info("Notification sent to " + EMAIL);
     }
 
     @Test
     void notifyUser_EmailException() throws EmailException {
+        doThrow(new EmailException("Email sending failed"))
+                .when(emailService).sendEmail(EMAIL, "Notification", MESSAGE);
+
+        // when(emailService.sendEmail(EMAIL, "Notification", MESSAGE))
+        // .thenThrow(new EmailException("Email sending failed"));
+
+        underTest.notifyUser(EMAIL, MESSAGE);
+        verify(emailService).sendEmail(EMAIL, "Notification", MESSAGE);
+        verify(logger).info("Attempting to send notification to " + EMAIL);
+        verify(logger).log(eq(Level.SEVERE), eq("Failed to send email to " + EMAIL),
+                any(EmailException.class));
 
     }
 }
