c40f0eb11f24db753be13b963a09a6369c714bb9
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-08 20:19:48 +0000

    Starter

---
+++ my-app/src/main/java/com/linkedin/app/NotificationService.java
@@ -0,0 +1,9 @@
+package com.linkedin.app;
+
+import java.sql.Timestamp;
+
+public class NotificationService {
+  public void send(String message, String recipient, Timestamp timestamp) {
+    // logic to send a notification
+  }
+}
--- my-app/src/test/java/com/linkedin/app/UserServiceTest.java
+++ my-app/src/test/java/com/linkedin/app/UserServiceTest.java
@@ -1,7 +1,15 @@
 package com.linkedin.app;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+import java.sql.Timestamp;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,13 +18,29 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class UserServiceTest {
 
     @Mock
-    private UserRepository userRepository;
+    private NotificationService notificationService;
 
     @InjectMocks
     private UserService underTest;
 
+    @Captor
+    private ArgumentCaptor<Timestamp> timestampCaptor;
+
     @Test
-    public void getUserById() throws Exception {
-        User mockUser = new User("123", "John Doe");
+    public void registerUser() {
+        String username = "john_doe";
+        String email = "john@example.com";
+
+        underTest.registerUser(username, email);
+
+        verify(notificationService).send(eq("Welcome " + username), eq(email), timestampCaptor.capture());
+
+        assertTrue(areWithinSeconds(new Timestamp(System.currentTimeMillis()), timestampCaptor.getValue(), 2));
+    }
+
+    private boolean areWithinSeconds(Timestamp timestamp1, Timestamp timestamp2, int seconds) {
+        long differenceInMillis = Math.abs(timestamp1.getTime() - timestamp2.getTime());
+        long differenceInSeconds = differenceInMillis / 1000;
+        return differenceInSeconds <= seconds;
     }
 }
