183574dd2f6f3a9f4548f0a306e66636a6571fe1
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-08 19:09:16 +0000

    Starter

--- my-app/src/test/java/com/linkedin/app/DatabaseServiceTest.java
+++ my-app/src/test/java/com/linkedin/app/DatabaseServiceTest.java
@@ -3,9 +3,12 @@ package com.linkedin.app;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DatabaseServiceTest {
 
     @Mock
