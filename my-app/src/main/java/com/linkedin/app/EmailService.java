2426c296e90bb5c3729dc051138e319e4f6e075c
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-08 18:56:31 +0000

    Testing starter

---
+++ my-app/src/main/java/com/linkedin/app/EmailService.java
@@ -0,0 +1,8 @@
+package com.linkedin.app;
+
+public class EmailService {
+  public void sendEmail(String recipient, String subject, String body) throws EmailException {
+    // Implementation for sending email
+    throw new EmailException("Email sending failed");
+  }
+}
