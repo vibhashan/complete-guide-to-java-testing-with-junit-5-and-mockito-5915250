b3a53cbf0fdb3aa8c133c4f58f89cfd43e394828
Kathryn Hodge <blondiebytes@gmail.com>
2024-08-08 17:26:05 +0000

    Implement

--- my-app/src/test/java/com/linkedin/app/TransactionServiceTest.java
+++ my-app/src/test/java/com/linkedin/app/TransactionServiceTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.app;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -8,6 +9,8 @@ import java.sql.Timestamp;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -21,13 +24,27 @@ public class TransactionServiceTest {
   @Mock
   private LoggerService loggerService;
 
+  @Captor
+  private ArgumentCaptor<Timestamp> timestampCaptor;
+
   @Test
   public void processTransaction() {
-    Transaction transaction = new Transaction("TX123", 10.50);
+    Timestamp start = new Timestamp(System.currentTimeMillis());
 
+    Transaction transaction = new Transaction("TX123", 10.50);
     underTest.processTransaction(transaction);
 
     verify(loggerService).logMessage(eq("TX123"), eq(10.50),
-        any(Timestamp.class));
+        timestampCaptor.capture());
+
+    Timestamp resultTimestamp = timestampCaptor.getValue();
+    System.out.println("Timestamp " + resultTimestamp);
+    assertTrue(areWithinSeconds(start, resultTimestamp, 5));
+  }
+
+  private boolean areWithinSeconds(Timestamp timestamp1, Timestamp timestamp2, int seconds) {
+    long differenceInMillis = Math.abs(timestamp1.getTime() - timestamp2.getTime());
+    long differenceInSeconds = differenceInMillis / 1000;
+    return differenceInSeconds <= seconds;
   }
 }
