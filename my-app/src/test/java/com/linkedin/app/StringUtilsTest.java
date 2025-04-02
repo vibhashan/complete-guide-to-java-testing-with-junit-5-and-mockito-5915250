e74cacdcf4ba88ae1470c46840d042b499d51278
vibhashan <ranhindavibhashana@gmail.com>
2025-04-02 22:35:25 +0530

    feat: 04-03 implemented

--- my-app/src/test/java/com/linkedin/app/StringUtilsTest.java
+++ my-app/src/test/java/com/linkedin/app/StringUtilsTest.java
@@ -3,32 +3,44 @@ package com.linkedin.app;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class StringUtilsTest {
+class StringUtilsTest {
+	@ParameterizedTest
+	@ValueSource(strings = { "", "a", "racecar", "RaceCar" })
+	void isPalindrome(String str) {
+		assertTrue(StringUtils.isPalindrome(str));
+	}
 
-  @Test
-  void isPalindromeWithEmptyString() {
-    assertTrue(StringUtils.isPalindrome(""));
-  }
+	@ParameterizedTest
+	@ValueSource(strings = { "hello" })
+	void isNotPalindrome(String str) {
+		assertFalse(StringUtils.isPalindrome(str));
+	}
 
-  @Test
-  void isPalindromeWithSingleCharacter() {
-    assertTrue(StringUtils.isPalindrome("a"));
-  }
-
-  @Test
-  void isPalindromeWithPalindromeString() {
-    assertTrue(StringUtils.isPalindrome("racecar"));
-  }
-
-  @Test
-  void isPalindromeWithNonPalindromeString() {
-    assertFalse(StringUtils.isPalindrome("hello"));
-  }
-
-  @Test
-  void isPalindromeWithMixedCasePalindromeString() {
-    assertTrue(StringUtils.isPalindrome("RaceCar"));
-  }
+//	@Test
+//	void isPalindromeWithEmptyString() {
+//		assertTrue(StringUtils.isPalindrome(""));
+//	}
+//
+//	@Test
+//	void isPalindromeWithSingleCharacter() {
+//		assertTrue(StringUtils.isPalindrome("a"));
+//	}
+//
+//	@Test
+//	void isPalindromeWithPalindromeString() {
+//		assertTrue(StringUtils.isPalindrome("racecar"));
+//	}
+//
+//	@Test
+//	void isPalindromeWithNonPalindromeString() {
+//		assertFalse(StringUtils.isPalindrome("hello"));
+//	}
+//
+//	@Test
+//	void isPalindromeWithMixedCasePalindromeString() {
+//		assertTrue(StringUtils.isPalindrome("RaceCar"));
+//	}
 }
