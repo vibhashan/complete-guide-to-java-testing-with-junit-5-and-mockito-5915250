fb098e4a421f7736ee53511bb3b36be6065ea883
vibhashan <ranhindavibhashana@gmail.com>
2025-04-02 22:58:34 +0530

    feat: 04-05 implemented

--- my-app/src/test/java/com/linkedin/app/SystemInfoTest.java
+++ my-app/src/test/java/com/linkedin/app/SystemInfoTest.java
@@ -3,34 +3,43 @@ package com.linkedin.app;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
-public class SystemInfoTest {
+class SystemInfoTest {
 
-  private final SystemInfo systemInfo = new SystemInfo();
+	private final SystemInfo systemInfo = new SystemInfo();
 
-  @Test
-  void getOSNameOnWindows() {
-    String osName = systemInfo.getOSName();
-    assertTrue(osName.contains("Windows"), "OS name should contain 'Windows'");
-  }
+	@Test
+	@EnabledOnOs({ OS.WINDOWS })
+	void getOSNameOnWindows() {
+		String osName = systemInfo.getOSName();
+		assertTrue(osName.contains("Windows"), "OS name should contain 'Windows'");
+	}
 
-  @Test
-  void getOSNameOnLinux() {
-    String osName = systemInfo.getOSName();
-    assertTrue(osName.contains("Linux"), "OS name should contain 'Linux'");
-  }
+	@Test
+	@EnabledOnOs({ OS.LINUX })
+	void getOSNameOnLinux() {
+		String osName = systemInfo.getOSName();
+		assertTrue(osName.contains("Linux"), "OS name should contain 'Linux'");
+	}
 
-  @Test
-  void getJavaVersionOnJava11() {
-    String javaVersion = systemInfo.getJavaVersion();
-    assertTrue(javaVersion.startsWith("11"), "Java version should start with '11'");
-  }
+	@Test
+	@EnabledOnJre(JRE.JAVA_11)
+	void getJavaVersionOnJava11() {
+		String javaVersion = systemInfo.getJavaVersion();
+		assertTrue(javaVersion.startsWith("11"), "Java version should start with '11'");
+	}
 
-  @Test
-  void getOSNameOnMacWithJava17() {
-    String osName = systemInfo.getOSName();
-    String javaVersion = systemInfo.getJavaVersion();
-    assertTrue(osName.contains("Mac"), "OS name should contain 'Mac'");
-    assertTrue(javaVersion.startsWith("17"), "Java version should start with '17'");
-  }
+	@Test
+	@EnabledOnOs(OS.MAC)
+	@EnabledOnJre(JRE.JAVA_17)
+	void getOSNameOnMacWithJava17() {
+		String osName = systemInfo.getOSName();
+		String javaVersion = systemInfo.getJavaVersion();
+		assertTrue(osName.contains("Mac"), "OS name should contain 'Mac'");
+		assertTrue(javaVersion.startsWith("17"), "Java version should start with '17'");
+	}
 }
