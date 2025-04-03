f5308cc2934588187b00f4b1cc432666f7c58293
vibhashan <ranhindavibhashana@gmail.com>
2025-04-03 12:47:09 +0530

    feat: 05-06 implemented

--- my-app/src/test/java/com/linkedin/app/CalculatorTest.java
+++ my-app/src/test/java/com/linkedin/app/CalculatorTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-  @Test
-  void add() {
-    Calculator calculator = new Calculator();
-    int result = calculator.add(2, 3);
+	@Test
+	void add() {
+		Calculator calculator = new Calculator();
+		int result = calculator.add(2, 3);
 
-    assertEquals(5, result, "Expected 5 but got " + result);
-  }
+		// Using of lambda expression (message suppliers) are more efficient since it
+		// only constructs the message for a failure
+		// preventing unnecessary object creation
+		assertEquals(5, result, () -> "Expected 5 but got " + result);
+	}
 }
