ee584e2364d52c0759b3bf28a1b2afda21d62b3d
vibhashan <ranhindavibhashana@gmail.com>
2025-04-03 12:20:50 +0530

    feat: 05-03 implemented

--- my-app/src/test/java/com/linkedin/app/TimeoutTest.java
+++ my-app/src/test/java/com/linkedin/app/TimeoutTest.java
@@ -1,12 +1,16 @@
 package com.linkedin.app;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class TimeoutTest {
 
-  @Test
-  void methodWithTimeout() throws InterruptedException {
-    // Perform test logic that should complete within 5 seconds
-    Thread.sleep(6000); // Simulating a lengthy operation
-  }
+	@Test
+	@Timeout(value = 5, unit = TimeUnit.SECONDS)
+	void methodWithTimeout() throws InterruptedException {
+		// Perform test logic that should complete within 5 seconds
+		Thread.sleep(4000); // Simulating a lengthy operation
+	}
 }
