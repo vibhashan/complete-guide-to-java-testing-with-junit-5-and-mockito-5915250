abc7e0d3b33b9c07881578ef9d117d0fc9ded6f9
vibhashan <ranhindavibhashana@gmail.com>
2025-04-07 11:24:54 +0530

    feat: 07-07 implemented

--- my-app/src/test/java/com/linkedin/app/UtilityTest.java
+++ my-app/src/test/java/com/linkedin/app/UtilityTest.java
@@ -1,11 +1,21 @@
 package com.linkedin.app;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
 
-public class UtilityTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UtilityTest {
 
     @Test
-    public void celsiusToFahrenheit() {
-
+    void celsiusToFahrenheit() {
+        try (MockedStatic<Utility> utility = mockStatic(Utility.class)) {
+            utility.when(() -> Utility.celsiusToFahrenheit(0)).thenReturn(32.0);
+            assertEquals(32.0, Utility.celsiusToFahrenheit(0));
+        }
     }
 }
